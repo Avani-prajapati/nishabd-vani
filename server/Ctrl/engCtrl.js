fd767c29ed1226d59efb4fdb1c7d45725fd2eedd
Amankumar-G <amangaloliya@gmail.com>
2024-11-24 16:32:33 +0530

    May be it is the final push of NishabdVani

--- server/Ctrl/engCtrl.js
+++ server/Ctrl/engCtrl.js
@@ -44,13 +44,7 @@ async function generateQuiz(currentAlphabet, alphabetSet) {
   return quiz;
 }
 
-
-
-
-
-
 // English alphabet route: Start Learning
-
 export const alphabetEng = wrapAsync(async (req, res) => {
   // Initialize session state if not already initialized
 
@@ -67,8 +61,6 @@ export const alphabetEng = wrapAsync(async (req, res) => {
 
   return res.json(data);
 });
-
-
 
 // Next alphabet route
 export const alphabetEngNext = wrapAsync(async (req, res) => {
@@ -95,7 +87,6 @@ export const alphabetEngNext = wrapAsync(async (req, res) => {
     return res.json(data);
   }
 });
-
 
 // Previous alphabet route
 export const alphabetEngPrev = wrapAsync(async (req, res) => {
